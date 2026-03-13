90e9661e9dbfe11ec2d155b024dabed6dee46da8
Paul Stack <public@paulstack.co.uk>
2026-03-13 16:23:00 +0000

    Update swamp version and SHA256 checksums
    Initial Release of the Tap

--- Formula/swamp.rb
+++ Formula/swamp.rb
@@ -1,27 +1,27 @@
   class Swamp < Formula
     desc "AI Native Automation CLI"
     homepage "https://swamp.club"
-    version "20260312.123456.0-sha.abcd1234" 
+    version "20260312.232506.0-sha.dfa7c4e9" 
 
     on_macos do
       on_intel do
         url "https://artifacts.systeminit.com/swamp/#{version}/binary/darwin/x86_64/swamp-#{version}-binary-darwin-x86_64.tar.gz"
-        sha256 "PLACEHOLDER"
+        sha256 "93ef919318dcbc4d2efc8a470283c47bf8405dddd9831255afe2a1a442d3bf51"
       end
       on_arm do
         url "https://artifacts.systeminit.com/swamp/#{version}/binary/darwin/aarch64/swamp-#{version}-binary-darwin-aarch64.tar.gz"
-        sha256 "PLACEHOLDER"
+        sha256 "6c6f40ceb6644f93ae1228f2825236fdb695bd5417ac5b42cddd55b0a35d81b1"
       end
     end
 
     on_linux do
       on_intel do
         url "https://artifacts.systeminit.com/swamp/#{version}/binary/linux/x86_64/swamp-#{version}-binary-linux-x86_64.tar.gz"
-        sha256 "PLACEHOLDER"
+        sha256 "302355dc7aa850be46e178d12d9427300c0395d950035588222bcc4f32029fd9"
       end
       on_arm do
         url "https://artifacts.systeminit.com/swamp/#{version}/binary/linux/aarch64/swamp-#{version}-binary-linux-aarch64.tar.gz"
-        sha256 "PLACEHOLDER"
+        sha256 "d048331a4a33be7412b16492a7adc149a0eaac44e125d8da67df180af30aec58"
       end
     end
 
